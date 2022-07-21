c0b1fd67ae0d2e77144557ee17d8923e4d7a39d7
swim0413 <chosuyoung0413@gmail.com>
2022-07-21 16:54:41 +0900

    master

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const fs = require('fs');
 const app = express();
 
 const PORT = 413;
-const hostName = '192.168.0.100';
+const hostName = '192.168.55.181';
 
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
@@ -33,18 +34,23 @@ app.get('/write', (req, res) => {
   res.render('write.ejs');
 });
 
-app.post('/write', async function(req, res){
-  var name = req.body.name;
-  var content = req.body.content;
-  var pw = req.body.passWord;
+const upload = multer({
+  dest: __dirname+'/views/pics/',
+});
+
+app.post('/write', upload.single('img'), async function(req, res){
+  let name = req.body.name;
+  let content = req.body.content;
+  let pw = req.body.passWord;
+  let img = req.file? req.file: null;
   if(name == '' || content == '' || pw == ''){
     res.render('write.ejs');
     return;
   }
-  var ip = req.headers['cf-connecting-ip'] || req.ip;
-  var D = new Date();
-  var date = D.toISOString();
-  db['data'].push({name,content,pw,ip,date});
+  let ip = req.headers['cf-connecting-ip'] || req.ip;
+  let D = new Date();
+  let date = D.toISOString();
+  db['data'].push({name,content,pw,ip,date, img:img?img.filename:"null.png"});
   fs.writeFile('./data/db.json', JSON.stringify(db), 'utf-8',function(err){
     if(err) return console.log('errrrr:'+err);
   });
@@ -53,8 +59,8 @@ app.post('/write', async function(req, res){
 });
 
 app.post('/del', async function(req, res){
-  var deletePW = req.body.getDate;
-  var dbIndex = db['data'].findIndex(obj => obj.date == deletePW);
+  let deletePW = req.body.getDate;
+  let dbIndex = db['data'].findIndex(obj => obj.date == deletePW);
   if(db['data'][dbIndex]['pw']==req.body.delete){
     db['data'].splice(dbIndex, 1);
     fs.writeFile('./data/db.json', JSON.stringify(db), 'utf-8',function(err){
@@ -66,6 +72,10 @@ app.post('/del', async function(req, res){
   }
   console.log(deletePW, dbIndex);
   res.redirect('/main');
+});
+
+app.get('/pics/:name', (req, res) => {
+  res.send('<img src="./views/pics/`${req.params.name}`">');
 });
 
 app.listen(PORT, hostName, () => {
